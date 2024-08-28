ba7f6c958ad6ed0de13622a61962a198284b3178
Dexter <dexter@dxlb.nl>
2024-08-28 13:16:43 +0200

    Fix home

---
+++ src/lib/QR.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+  import { browser } from "$app/environment";
+  const props = $props();
+
+  let QRX = $state();
+
+  onMount(async () => {
+    if (browser) {
+      QRX = (await import("@qr-x/svelte")).default;
+    }
+  });
+</script>
+
+{#if QRX}
+  <QRX {...props} />
+{/if}
